042a81fcef5dd55c1a93510fae621449fe95354f
Rafael Simon Maia <rafaelsimonmaia@gmail.com>
2017-02-20 15:15:58 -0700

    Second assignment

--- function.py
+++ function.py
@@ -1,31 +1,18 @@
-def cross(a, b):
-    "Cross product of elements in A and elements in B."
-    return [s + t for s in a for t in b]
+from utils import *
 
+def grid_values(grid):
+    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
 
-rows = 'ABCDEFGHI'
-cols = '123456789'
-boxes = cross(rows, cols)
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+    """
+    values = [value if value != '.' else cols for value in grid] # turn grid into array of values
+    gridValues = dict(list(zip(boxes,values))) # turn boxes and values in a list then a dictionary
+    return gridValues
 
-row_units = [cross(r, cols) for r in rows]
-# Element example:
-# row_units[0] = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
-# This is the top most row.
-
-column_units = [cross(rows, c) for c in cols]
-# Element example:
-# column_units[0] = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1']
-# This is the left most column.
-
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-# Element example:
-# square_units[0] = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
-# This is the top left square.
-
-unitlist = row_units + column_units + square_units
-
-
-units = dict((s, [u for u in unitlist if s in u])
-             for s in squares)
-peers = dict((s, set(sum(units[s],[]))-set([s]))
-             for s in squares)
+if __name__ == '__main__':
+    display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
