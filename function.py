b46ff41e869365f39a5a7f45d75fb891aacc2314
Rafael Simon Maia <rafaelsimonmaia@gmail.com>
2017-02-20 15:02:45 -0700

    First assignment

---
+++ function.py
@@ -0,0 +1,31 @@
+def cross(a, b):
+    "Cross product of elements in A and elements in B."
+    return [s + t for s in a for t in b]
+
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+# Element example:
+# row_units[0] = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
+# This is the top most row.
+
+column_units = [cross(rows, c) for c in cols]
+# Element example:
+# column_units[0] = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1']
+# This is the left most column.
+
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+# Element example:
+# square_units[0] = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+# This is the top left square.
+
+unitlist = row_units + column_units + square_units
+
+
+units = dict((s, [u for u in unitlist if s in u])
+             for s in squares)
+peers = dict((s, set(sum(units[s],[]))-set([s]))
+             for s in squares)
